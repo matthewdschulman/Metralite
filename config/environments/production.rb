9a3748c2cfebd93eea2b0f409b9159f55e737edf
Matt Schulman <matthewdschulman@gmail.com>
2014-11-11 11:30:48 -0500

    update initializers

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,14 +20,12 @@ Metralite::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -80,5 +78,6 @@ Metralite::Application.configure do
 
   config.serve_static_assets = true
   config.assets.compile = true
-  config.assets.digest = true
+
+
 end
