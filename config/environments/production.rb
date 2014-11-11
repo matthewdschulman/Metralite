ba651deb6ef4b9ac5c3605cabcc4cc572968aad1
Matt Schulman <matthewdschulman@gmail.com>
2014-11-11 11:17:38 -0500

    undo precompile changes

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,4 +1,4 @@
-Shopnplot::Application.configure do
+Metralite::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
@@ -20,14 +20,14 @@ Shopnplot::Application.configure do
   # config.action_dispatch.rack_cache = true
 
   # Disable Rails's static asset server (Apache or nginx will already do this).
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
@@ -77,5 +77,8 @@ Shopnplot::Application.configure do
 
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
-  config.assets.precompile += ['static_pages.css']
+
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
 end
